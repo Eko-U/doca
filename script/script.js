f96ea4ce6477732fe802af2c39c21d86e4407b54
Eko-U <ekogift2000@gmail.com>
2022-06-24 09:33:17 +0100

    Added some functionality

--- script/script.js
+++ script/script.js
@@ -1,12 +1,45 @@
 const navMenu = document.querySelector('.nav__logo-icon');
 const sideBar = document.querySelector('.sidebar');
 const closeBar = document.querySelector('.icon__close');
+const inputName = document.querySelector('.newsletter__input')
+const btn = document.querySelector('.btn-form')
+const nameError = document.querySelector('.nameError');
+const emailError = document.querySelector('.emailError')
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
+const messageError = document.querySelector('.messageError');
 
 navMenu.addEventListener('click', function(){
-	sideBar.classList.remove('hidden');
+	sideBar.classList.toggle('hidden');
 	console.log('sidebar is click');
 });
 
-closeBar.addEventListener('click', function(){
-	sideBar.classList.add('hidden');
+
+// HOME FORM BUTTON
+
+console.log(btn);
+btn.addEventListener('click', (e) => {
+	if (inputName.value === '' || inputName.value === null) {
+		e.preventDefault();
+		nameError.textContent = 'Please Enter Your Username 😊';
+	}else if(inputName.value.length > 20){
+		e.preventDefault();
+		nameError.textContent = 'Name too long 🙂🙂'
+	}else if(inputName.value.length > 0) {
+		nameError.style.display = 'none';
+	}
+	
+	if (email.value === '' || email.value === null) {
+		emailError.textContent = 'Please Enter Your email 😊';
+	}else if (email.value.length > 0) {
+		emailError.style.display = 'none';
+	}
 });
+
+
+message.addEventListener('input', function(e){
+});
+
+if (message.value < 6) {
+		messageError.textContent = 'Message is too short. Please try send us more messages';
+}
